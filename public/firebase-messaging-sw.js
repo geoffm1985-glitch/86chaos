e9d63ca611bda87346fa369f9cc510bd41f626d7
geoffm1985-glitch <geoffm1985@gmail.com>
2026-06-10 01:03:32 -0500

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA0kkmRCqGNoB1LXKfuCNIl1JKDyQci9hA",
+  authDomain: "cheers-34b8d.firebaseapp.com",
+  projectId: "cheers-34b8d",
+  storageBucket: "cheers-34b8d.firebasestorage.app",
+  messagingSenderId: "762225019248",
+  appId: "1:762225019248:web:3e142c9563e58ca762a7b5"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/app-icon.png',
+    badge: '/app-icon.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
