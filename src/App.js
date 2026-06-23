e5cd5e553b8fa63197d0e880d65d76bcb5d4417a
geoffm1985-glitch <geoffm1985@gmail.com>
2026-06-22 22:55:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,12 +23,13 @@ const T = {
 
 // --- Firebase Initialization ---
 const firebaseConfig = {
-  apiKey: "AIzaSyBTyx_RzxArATLaPwmtdKC8TSdBGusl0cQ",
-  authDomain: "cheers-sync-manager.firebaseapp.com",
-  projectId: "cheers-sync-manager",
-  storageBucket: "cheers-sync-manager.firebasestorage.app",
-  messagingSenderId: "810480537904",
-  appId: "1:810480537904:web:8f95be54456e6e6a85f8f5"
+  apiKey: "AIzaSyA0kkmRCqGNoB1LXKfuCNIl1JKDyQci9hA",
+  authDomain: "cheers-34b8d.firebaseapp.com",
+  projectId: "cheers-34b8d",
+  storageBucket: "cheers-34b8d.firebasestorage.app",
+  messagingSenderId: "762225019248",
+  appId: "1:762225019248:web:3e142c9563e58ca762a7b5",
+  measurementId: "G-JFZ6EZB0E3"
 };
 
 const app = initializeApp(firebaseConfig);
