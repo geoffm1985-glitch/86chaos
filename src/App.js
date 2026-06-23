4a39a36ad8e601ded07b80dff97fa69af6dde077
geoffm1985-glitch <geoffm1985@gmail.com>
2026-06-22 20:37:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ const T = {
 
 // --- Firebase Initialization ---
 const firebaseConfig = {
-  apiKey: "AIzaSyA0kkmRCqGNoB1LXKfuCNIl1JKDyQci9hA",
-  authDomain: "cheers-34b8d.firebaseapp.com",
-  projectId: "cheers-34b8d",
-  storageBucket: "cheers-34b8d.firebasestorage.app",
-  messagingSenderId: "762225019248",
-  appId: "1:762225019248:web:3e142c9563e58ca762a7b5"
+  apiKey: "AIzaSyBIRGMeLnVE3w3i1WZJzurcp-LkeaNZ3hw",
+  authDomain: "chaos-test-d1601.firebaseapp.com",
+  projectId: "chaos-test-d1601",
+  storageBucket: "chaos-test-d1601.firebasestorage.app",
+  messagingSenderId: "534993379994",
+  appId: "1:534993379994:web:9fefb6e10309223afe7523"
 };
 
 const app = initializeApp(firebaseConfig);
