ad27b026fd4904106f7e790f94660a9d0062104f
geoffm1985-glitch <geoffm1985@gmail.com>
2026-06-23 11:35:23 -0500

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,25 +1,26 @@
-importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyA0kkmRCqGNoB1LXKfuCNIl1JKDyQci9hA",
   authDomain: "cheers-34b8d.firebaseapp.com",
   projectId: "cheers-34b8d",
   storageBucket: "cheers-34b8d.firebasestorage.app",
   messagingSenderId: "762225019248",
-  appId: "1:762225019248:web:3e142c9563e58ca762a7b5"
-});
+  appId: "1:762225019248:web:3e142c9563e58ca762a7b5",
+  measurementId: "G-JFZ6EZB0E3"
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
     body: payload.notification.body,
-    icon: '/app-icon.png',
-  });
+    icon: '/logo192.png',
+    badge: '/logo192.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
-
-// Force the service worker to activate instantly without waiting for a page reload
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
