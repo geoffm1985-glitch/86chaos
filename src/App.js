17e271f92c5036ebd10bd8df8ce154bba44b6112
geoffm1985-glitch <geoffm1985@gmail.com>
2026-06-23 22:15:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,18 @@ const T = {
 };
 
 // --- Firebase Initialization ---
-const firebaseConfig = {
+// 1. TEST DATABASE CONFIG (Sandbox)
+const testConfig = {
+  apiKey: "AIzaSyBIRGMeLnVE3w3i1WZJzurcp-LkeaNZ3hw",
+  authDomain: "chaos-test-d1601.firebaseapp.com",
+  projectId: "chaos-test-d1601",
+  storageBucket: "chaos-test-d1601.firebasestorage.app",
+  messagingSenderId: "534993379994",
+  appId: "1:534993379994:web:9fefb6e10309223afe7523"
+};
+
+// 2. MAIN PRODUCTION DATABASE CONFIG (Live Data)
+const prodConfig = {
   apiKey: "AIzaSyA0kkmRCqGNoB1LXKfuCNIl1JKDyQci9hA",
   authDomain: "cheers-34b8d.firebaseapp.com",
   projectId: "cheers-34b8d",
@@ -31,6 +42,9 @@ const firebaseConfig = {
   appId: "1:762225019248:web:3e142c9563e58ca762a7b5",
   measurementId: "G-JFZ6EZB0E3"
 };
+
+// 3. THE SWITCHER (Automatically routes the database based on the URL)
+const firebaseConfig = window.location.hostname === 'app.86chaos.com' ? prodConfig : testConfig;
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
