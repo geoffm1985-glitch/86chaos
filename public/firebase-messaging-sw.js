6bb5b825d5529eb22206b9a51b3a2e0f14b4630c
geoffm1985-glitch <geoffm1985@gmail.com>
2026-07-01 00:51:07 -0500

    Refactor Firebase config for test and production

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,18 @@
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
-// Hardcoded LIVE PRODUCTION Database Credentials
-const firebaseConfig = {
+// 1. TEST DATABASE CONFIG (Sandbox)
+const testConfig = {
+  apiKey: "AIzaSyBIRGMeLnVE3w3i1WZJzurcp-LkeaNZ3hw",
+  authDomain: "chaos-test-d1601.firebaseapp.com",
+  projectId: "chaos-test-d1601",
+  storageBucket: "chaos-test-d1601.firebasestorage.app",
+  messagingSenderId: "534993379994",
+  appId: "1:534993379994:web:9fefb6e10309223afe7523"
+};
+
+// 2. MAIN PRODUCTION DATABASE CONFIG (Live Data)
+const prodConfig = {
   apiKey: "AIzaSyA0kkmRCqGNoB1LXKfuCNIl1JKDyQci9hA",
   authDomain: "cheers-34b8d.firebaseapp.com",
   projectId: "cheers-34b8d",
@@ -11,6 +21,9 @@ const firebaseConfig = {
   appId: "1:762225019248:web:3e142c9563e58ca762a7b5",
   measurementId: "G-JFZ6EZB0E3"
 };
+
+// 3. THE SMART BACKGROUND SWITCHER
+const firebaseConfig = self.location.hostname === 'app.86chaos.com' ? prodConfig : testConfig;
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
