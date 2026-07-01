a053ee88984adbd85afdcb9e2d9effd0e67a6e39
geoffm1985-glitch <geoffm1985@gmail.com>
2026-06-30 19:36:36 -0500

    Upgrade Firebase SDK to version 10.8.0

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,26 +1,25 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA0kkmRCqGNoB1LXKfuCNIl1JKDyQci9hA",
   authDomain: "cheers-34b8d.firebaseapp.com",
   projectId: "cheers-34b8d",
   storageBucket: "cheers-34b8d.firebasestorage.app",
   messagingSenderId: "762225019248",
-  appId: "1:762225019248:web:3e142c9563e58ca762a7b5",
-  measurementId: "G-JFZ6EZB0E3"
+  appId: "1:762225019248:web:3e142c9563e58ca762a7b5"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/logo192.png',
-    badge: '/logo192.png'
+    icon: '/wisco.png'
   };
+
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
