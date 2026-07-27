ad7c9bfdfc6da30ef07e56c25919a263beec0576
geoffm1985-glitch <geoffm1985@gmail.com>
2026-07-27 18:37:30 -0500

    Fix client test environment and schedule planner expectations

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,37 @@
-const { TextDecoder, TextEncoder } = require('util');
+const util = require('util');
 
-if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = TextEncoder;
+if (typeof globalThis.TextEncoder === 'undefined' && util.TextEncoder) {
+  globalThis.TextEncoder = util.TextEncoder;
 }
 
-if (typeof global.TextDecoder === 'undefined') {
-  global.TextDecoder = TextDecoder;
+if (typeof globalThis.TextDecoder === 'undefined' && util.TextDecoder) {
+  globalThis.TextDecoder = util.TextDecoder;
 }
+
+try {
+  const webStreams = require('stream/web');
+
+  if (typeof globalThis.ReadableStream === 'undefined' && webStreams.ReadableStream) {
+    globalThis.ReadableStream = webStreams.ReadableStream;
+  }
+
+  if (typeof globalThis.WritableStream === 'undefined' && webStreams.WritableStream) {
+    globalThis.WritableStream = webStreams.WritableStream;
+  }
+
+  if (typeof globalThis.TransformStream === 'undefined' && webStreams.TransformStream) {
+    globalThis.TransformStream = webStreams.TransformStream;
+  }
+} catch (error) {
+  // Older Node/Jest environments may not expose stream/web.
+}
+
+jest.mock('firebase/messaging', () => ({
+  getMessaging: jest.fn(() => ({
+    app: null,
+    name: '[jest-mocked-messaging]'
+  })),
+  isSupported: jest.fn(() => Promise.resolve(false)),
+  getToken: jest.fn(() => Promise.resolve(null)),
+  onMessage: jest.fn(() => () => {})
+}));
