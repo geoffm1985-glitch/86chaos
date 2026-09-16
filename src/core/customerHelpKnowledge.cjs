d4e87df6d3207999988060bad7e4abdbad89e01d
geoffm1985-glitch <geoffm1985@gmail.com>
2026-09-15 21:30:00 -0500

    Release 16.0.235: repair mobile schedule PDF delivery

--- src/core/customerHelpKnowledge.cjs
+++ src/core/customerHelpKnowledge.cjs
@@ -1,6 +1,6 @@
 'use strict';
 
-const CUSTOMER_HELP_VERSION = '16.0.234';
+const CUSTOMER_HELP_VERSION = '16.0.235';
 
 const AUDIENCE = Object.freeze({ EVERYONE: 'Everyone', EMPLOYEE: 'Employee', MANAGER_OWNER: 'Manager / Owner' });
 
